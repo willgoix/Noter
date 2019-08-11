2f108043d080a3a961d8f54654b085e46bf74bf5
Willian Gois <willian2o12@hotmail.com>
2019-08-10 23:48:52 -0300

    :sparkles: Add password encryptation

---
+++ src/com/noter/util/PasswordUtils.java
@@ -0,0 +1,58 @@
+package com.noter.util;
+
+import javax.crypto.SecretKeyFactory;
+import javax.crypto.spec.PBEKeySpec;
+
+import java.security.NoSuchAlgorithmException;
+import java.security.SecureRandom;
+import java.security.spec.InvalidKeySpecException;
+import java.util.Arrays;
+import java.util.Base64;
+import java.util.Random;
+
+/**
+ * @author Willian Gois (github/willgoix)
+ */
+public class PasswordUtils {
+
+    private static final Random RANDOM = new SecureRandom();
+    private static final String ALPHABET = "0123456789ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz";
+    private static final int ITERATIONS = 10000;
+    private static final int KEY_LENGTH = 256;
+
+    public static String getSalt(int length) {
+        StringBuilder returnValue = new StringBuilder(length);
+        for (int i = 0; i < length; i++) {
+            returnValue.append(ALPHABET.charAt(RANDOM.nextInt(ALPHABET.length())));
+        }
+        return new String(returnValue);
+    }
+
+    public static byte[] hash(char[] password, byte[] salt) {
+        PBEKeySpec spec = new PBEKeySpec(password, salt, ITERATIONS, KEY_LENGTH);
+        Arrays.fill(password, Character.MIN_VALUE);
+
+        try {
+            SecretKeyFactory skf = SecretKeyFactory.getInstance("PBKDF2WithHmacSHA1");
+
+            return skf.generateSecret(spec).getEncoded();
+        } catch (NoSuchAlgorithmException | InvalidKeySpecException e) {
+            throw new AssertionError("Error while hashing a password: " + e.getMessage(), e);
+        } finally {
+            spec.clearPassword();
+        }
+    }
+
+    public static String generateSecurePassword(String password, String salt) {
+        byte[] securePasswordBytes = hash(password.toCharArray(), salt.getBytes());
+        String securePassword = Base64.getEncoder().encodeToString(securePasswordBytes);
+
+        return securePassword;
+    }
+
+    public static boolean verifyUserPassword(String password, String passwordHash, String salt) {
+        String newSecurePassword = generateSecurePassword(password, salt);
+
+        return newSecurePassword.equalsIgnoreCase(passwordHash);
+    }
+}
